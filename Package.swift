eb1407888dd1df82df17133ff08e61ab42a5f9f9
zpbc007 <719197352@qq.com>
2024-05-22 10:12:51 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swiftUI-keyboard-editor",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swiftUI-keyboard-editor",
+            targets: ["swiftUI-keyboard-editor"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "swiftUI-keyboard-editor"),
+        .testTarget(
+            name: "swiftUI-keyboard-editorTests",
+            dependencies: ["swiftUI-keyboard-editor"]),
+    ]
+)
---
+++ Tests/swiftUI-keyboard-editorTests/swiftUI_keyboard_editorTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swiftUI_keyboard_editor
+
+final class swiftUI_keyboard_editorTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
