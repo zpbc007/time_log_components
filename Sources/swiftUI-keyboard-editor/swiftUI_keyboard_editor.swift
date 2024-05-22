0cb5de09b96e036e213d16ca969c3b4bba783636
zpbc007 <719197352@qq.com>
2024-05-22 10:44:13 +0800

    feat: init

--- Sources/swiftUI-keyboard-editor/swiftUI_keyboard_editor.swift
+++ Sources/swiftUI-keyboard-editor/swiftUI_keyboard_editor.swift
@@ -1,2 +1,29 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+struct KeyboardEditor: View {
+    @State private var text = "xxxx"
+    @FocusState var focused: Bool
+    
+    var body: some View {
+        TextEditor(text: $text)
+            .focused($focused)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("xxx") {
+                        print("click")
+                    }
+                }
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                    focused = true
+                }
+            }
+    }
+}
+
+#Preview {
+    KeyboardEditor()
+}
