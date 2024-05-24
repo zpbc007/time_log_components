2bb6a0d2a7bb5d21575ff538e63d5d226f5e3bc8
zpbc007 <719197352@qq.com>
2024-05-24 18:52:12 +0800

    update

--- Sources/swiftUI-keyboard-editor/swiftUI_keyboard_editor.swift
+++ Sources/swiftUI-keyboard-editor/swiftUI_keyboard_editor.swift
@@ -1,31 +1,99 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import SwiftUI
+import Combine
 
 public struct KeyboardEditor: View {
-    @State private var text = "xxxx"
-    @FocusState private var focused: Bool
+    @State private var editorText = "editor text"
+    @State private var title = ""
+    @State private var desc = ""
     
     public init() {}
     
     public var body: some View {
-        TextEditor(text: $text)
-            .focused($focused)
-            .toolbar {
-                ToolbarItemGroup(placement: .keyboard) {
-                    Button("xxx") {
-                        print("click")
-                    }
-                }
-            }
-            .onAppear() {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-                    focused = true
-                }
-            }
+        VStack {
+            Spacer()
+            
+            ToolbarContent(title: $title, desc: $desc)
+        }
+//        .KeyboardAwarePadding()
     }
 }
 
+struct ToolbarContent: View {
+    enum Field: Hashable {
+        case title
+        case desc
+    }
+    @Binding var title: String
+    @Binding var desc: String
+    @FocusState private var focusedField: Field?
+    
+    var body: some View {
+        Form {
+            TextField("标题", text: $title)
+                .focused($focusedField, equals: .title)
+            
+            TextField("描述", text: $desc, axis: .vertical)
+                .lineLimit(5...10)
+                .focused($focusedField, equals: .desc)
+                        
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "tag")
+                }
+                
+                Spacer()
+                
+                Button {
+                    focusedField = nil
+                } label: {
+                    Image(systemName: "keyboard.chevron.compact.down")
+                }
+            }
+        }
+        .onAppear() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                focusedField = .title
+            }
+        }
+    }
+}
+
+struct KeyboardAwareModifier: ViewModifier {
+    @State private var keyboardHeight: CGFloat = 0
+
+    private var keyboardHeightPublisher: AnyPublisher<CGFloat, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+                .map { $0.cgRectValue.height },
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+       ).eraseToAnyPublisher()
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, keyboardHeight)
+            .onReceive(keyboardHeightPublisher) { self.keyboardHeight = $0 }
+    }
+}
+
+extension View {
+    func KeyboardAwarePadding() -> some View {
+        ModifiedContent(content: self, modifier: KeyboardAwareModifier())
+    }
+}
+
+//#Preview {
+//    KeyboardEditor()
+//}
+
 #Preview {
-    KeyboardEditor()
+    ToolbarContent(title: .constant("title"), desc: .constant("desc"))
 }
