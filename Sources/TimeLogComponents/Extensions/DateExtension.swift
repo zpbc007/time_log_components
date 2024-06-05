1147cf188545ec199a4c094881d490ac0b58c95c
zpbc007 <719197352@qq.com>
2024-06-05 15:33:16 +0800

    feat: add weekFirstDay

--- Sources/TimeLogComponents/Extensions/DateExtension.swift
+++ Sources/TimeLogComponents/Extensions/DateExtension.swift
@@ -11,15 +11,7 @@ extension Date {
     /// 获取 date 所在周的所有日期
     public func weekDays() -> [Date] {
         var result: [Date] = .init()
-
-        guard let startOfWeek = Calendar.current.date(
-            from: Calendar.current.dateComponents(
-                [.yearForWeekOfYear, .weekOfYear],
-                from: self
-            )
-        ) else {
-            return result
-        }
+        let startOfWeek = self.weekFirstDay(calendar: .current)
 
         (0...6).forEach { day in
             if let weekday = Calendar.current.date(
@@ -32,6 +24,15 @@ extension Date {
         }
 
         return result
+    }
+    
+    public func weekFirstDay(calendar: Calendar) -> Date {
+        calendar.date(
+            from: Calendar.current.dateComponents(
+                [.yearForWeekOfYear, .weekOfYear],
+                from: self
+            )
+        ) ?? self
     }
     
     public func toString(format: String) -> String {
