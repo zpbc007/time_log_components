829c55cc150db17a24050cb536d94ee5733c73ba
zpbc007 <719197352@qq.com>
2024-06-06 14:24:19 +0800

    feat: add TodayOverview

---
+++ Sources/TimeLogComponents/UIComponents/TodayOverview.swift
@@ -0,0 +1,63 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by zhaopeng on 2024/6/6.
+//
+
+import SwiftUI
+
+public struct TodayOverview: View {
+    static let formatter: DateComponentsFormatter = {
+        let formater = DateComponentsFormatter()
+        formater.allowedUnits = [.hour, .minute, .second]
+        formater.unitsStyle = .abbreviated
+        
+        return formater
+    }()
+    
+    public init(recordTime: Int, recordSeconds: TimeInterval) {
+        self.recordTime = recordTime
+        self.recordSeconds = recordSeconds
+    }
+    
+    // 记录次数
+    let recordTime: Int
+    // 已记录时间
+    let recordSeconds: TimeInterval
+    
+    private var recordSecondsString: String? {
+        Self.formatter.string(from: recordSeconds)
+    }
+    
+    public var body: some View {
+        VStack {
+            HStack {
+                Text("已记录")
+                Text(recordTime, format: .number)
+                    .bold()
+                Text("次,")
+                
+                if let recordSecondsString {
+                    Text("共")
+                    Text(recordSecondsString)
+                        .bold()
+                }
+                
+                Spacer()
+            }.font(.caption)
+            
+            
+            ProgressView(value: recordSeconds, total: 24 * 60 * 60)
+                .tint(.green)
+                .scaleEffect(x: 1, y: 4, anchor: .center)
+        }
+    }
+}
+
+#Preview {
+    TodayOverview(
+        recordTime: 20,
+        recordSeconds: 10 * 60 * 60
+    )
+}
