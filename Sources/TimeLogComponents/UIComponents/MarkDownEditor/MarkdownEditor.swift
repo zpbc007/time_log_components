91f1fd355d770fdaaf91efef6ee88330fdf3fa2b
zpbc007 <719197352@qq.com>
2024-07-09 15:34:26 +0800

    fix: auto zoom

--- Sources/TimeLogComponents/UIComponents/MarkDownEditor/MarkdownEditor.swift
+++ Sources/TimeLogComponents/UIComponents/MarkDownEditor/MarkdownEditor.swift
@@ -75,7 +75,7 @@ public struct MarkdownEditor: UIViewRepresentable {
         return """
         <!DOCTYPE html>
         <html>
-        <head><meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"></head>
+        <head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, shrink-to-fit=no"></head>
         <style>\(finalCssString)</style>
         <body>
         <div id="info">
