9f5a8759516a8d734390dcd07c6d0f0fa11c98a7
zpbc007 <719197352@qq.com>
2024-07-19 14:51:54 +0800

    feat: add SettingPage

--- Sources/TimeLogComponents/BusinessComponents/SettingPage.swift
+++ Sources/TimeLogComponents/BusinessComponents/SettingPage.swift
@@ -7,12 +7,99 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct SettingPage: View {
+    let user: FeedbackView.UserInfo?
+    
+    public init(user: FeedbackView.UserInfo?) {
+        self.user = user
+    }
+    
+    public var body: some View {
+        Form {
+            UserInfoView(user: user)
+            
+            NavigationLink {
+                HelpCenterWebView()
+            } label: {
+                Label("新手指南", systemImage: "questionmark.circle")
+            }
+            
+            NavigationLink {
+                FeedbackView(user: user)
+            } label: {
+                Label("联系我们", systemImage: "phone.circle")
+            }
+        }
+    }
+}
+
+extension SettingPage {
+    struct UserInfoView: View {
+        let user: FeedbackView.UserInfo?
+        
+        var body: some View {
+            HStack {
+                if let photoUrl = user?.photoUrl, !photoUrl.isEmpty {
+                    AsyncImageLoader(urlString: photoUrl)
+                        .padding(.trailing, 10)
+                }
+                
+                Text(user?.displayName ?? "")
+            }
+        }
+    }
+}
+
+extension SettingPage {
+    struct AsyncImageLoader: View {
+        let urlString:  String
+        
+        private let logger = TLLogger(context: String(describing: SettingPage.AsyncImageLoader.self))
+        @State private var image: Image?
+        
+        var body: some View {
+            VStack {
+                if let image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .frame(width: 50, height: 50)
+                }
+            }.task {
+                loadImage()
+            }
+        }
+        
+        private func loadImage() {
+            guard let url = URL(string: urlString) else {
+                return
+            }
+            
+            Task {
+                do {
+                    let (imageData, _) = try await URLSession.shared.data(from: url)
+                    if let uiImage = UIImage(data: imageData) {
+                        DispatchQueue.main.async {
+                            image = Image(uiImage: uiImage)
+                        }
+                    }
+                } catch(let error) {
+                    logger.error("fetch image for url: \(urlString) error: \(error)")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    NavigationStack {
+        SettingPage(
+            user: .init(
+                uid: UUID().uuidString,
+                displayName: "用户名 XXX",
+                photoUrl: "https://slab.com/static/b5ae12a602adf067eb2373415281d9fe/7aa54/banner.webp"
+            )
+        )
+    }
 }
