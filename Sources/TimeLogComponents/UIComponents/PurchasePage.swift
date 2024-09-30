007680b05048de98e1aae843759d88a76852f000
zpbc007 <719197352@qq.com>
2024-09-30 12:45:07 +0800

    fix: remove contactAction

--- Sources/TimeLogComponents/UIComponents/PurchasePage.swift
+++ Sources/TimeLogComponents/UIComponents/PurchasePage.swift
@@ -14,7 +14,6 @@ public struct PurchasePage: View {
     let monthlyAction: () -> Void
     let yearlyAction: () -> Void
     let restoreAction: () -> Void
-    let contactAction: () -> Void
     
     public init(
         features: [FeatureConf],
@@ -22,8 +21,7 @@ public struct PurchasePage: View {
         yearlyConf: ButtonConf,
         monthlyAction: @escaping () -> Void,
         yearlyAction: @escaping () -> Void,
-        restoreAction: @escaping () -> Void,
-        contactAction: @escaping () -> Void
+        restoreAction: @escaping () -> Void
     ) {
         self.features = features
         self.monthlyConf = monthlyConf
@@ -31,7 +29,6 @@ public struct PurchasePage: View {
         self.monthlyAction = monthlyAction
         self.yearlyAction = yearlyAction
         self.restoreAction = restoreAction
-        self.contactAction = contactAction
     }
     
     public var body: some View {
@@ -55,9 +52,7 @@ public struct PurchasePage: View {
             }
             .scrollContentBackground(.hidden)
             .listStyle(.plain)
-            
-            Spacer()
-            
+                        
             HStack {
                 Text("购买说明")
                     .font(.title2)
@@ -75,7 +70,13 @@ public struct PurchasePage: View {
                 
                 HStack {
                     Text("3. 如果有其他问题请")
-                    Button("联系我们", action: contactAction)
+                    
+                    NavigationLink {
+                        FeedbackView(user: nil)
+                            .toolbar(.hidden, for: .tabBar)
+                    } label: {
+                        Text("联系我们")
+                    }
                 }
             }
             .font(.footnote)
@@ -137,26 +138,25 @@ extension PurchasePage {
 }
 
 #Preview {
-    PurchasePage(
-        features: [
-            .init(text: "iCloud 同步", desc: "在多设备间同步数据"),
-            .init(text: "清单解锁", desc: "无限创建清单"),
-            .init(text: "标签", desc: "无限创建标签"),
-            .init(text: "更多功能", desc: "努力开发中，敬请期待")
-        ],
-        monthlyConf: .init(price: 6.2, fontColor: .green, bgColor: .clear),
-        yearlyConf: .init(price: 38.9, fontColor: .white, bgColor: .green),
-        monthlyAction: {
-            print("monthlyAction")
-        },
-        yearlyAction: {
-            print("yearlyAction")
-        },
-        restoreAction: {
-            print("restoreAction")
-        },
-        contactAction: {
-            print("contactAction")
-        }
-    )
+    NavigationStack {
+        PurchasePage(
+            features: [
+                .init(text: "iCloud 同步", desc: "在多设备间同步数据"),
+                .init(text: "清单解锁", desc: "无限创建清单"),
+                .init(text: "标签", desc: "无限创建标签"),
+                .init(text: "更多功能", desc: "努力开发中，敬请期待")
+            ],
+            monthlyConf: .init(price: 6.2, fontColor: .green, bgColor: .clear),
+            yearlyConf: .init(price: 38.9, fontColor: .white, bgColor: .green),
+            monthlyAction: {
+                print("monthlyAction")
+            },
+            yearlyAction: {
+                print("yearlyAction")
+            },
+            restoreAction: {
+                print("restoreAction")
+            }
+        )
+    }
 }
