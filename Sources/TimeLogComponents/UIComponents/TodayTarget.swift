fe6559b2e9a29f52c01f0d517c50381b31160917
zpbc007 <719197352@qq.com>
2024-10-24 11:22:15 +0800

    feat: add TodayTarget

--- Sources/TimeLogComponents/UIComponents/TodayTarget.swift
+++ Sources/TimeLogComponents/UIComponents/TodayTarget.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct TodayTarget: View {
+    let comment: String
+    
+    public init(comment: String) {
+        self.comment = comment
+    }
+    
+    public var body: some View {
+        RichTextViewer(content: comment)
+            .padding()
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10))
+            .frame(maxHeight: 180)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    struct Playground: View {
+        @State private var comment: String = "{\"ops\":[{\"attributes\":{\"bold\":true},\"insert\":\"今天的目标是：\"},{\"insert\":\"\\n完成今日目标的开发\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"给菲打个视频\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"}]}"
+        
+        var body: some View {
+            VStack {
+                TodayTarget(comment: comment)
+            }.padding()
+        }
+    }
+    
+    return Playground()
 }
