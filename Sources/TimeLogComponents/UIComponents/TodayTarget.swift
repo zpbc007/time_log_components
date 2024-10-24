0551cc406832c1d57d28457303ce35e580289989
zpbc007 <719197352@qq.com>
2024-10-24 15:32:30 +0800

    feat: add empty state

--- Sources/TimeLogComponents/UIComponents/TodayTarget.swift
+++ Sources/TimeLogComponents/UIComponents/TodayTarget.swift
@@ -15,23 +15,31 @@ public struct TodayTarget: View {
     }
     
     public var body: some View {
-        RichTextViewer(content: comment)
-            .padding()
-            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10))
-            .frame(maxHeight: 180)
+        Group {
+            if comment.isEmpty {
+                HStack {
+                    Spacer()
+                    Image(systemName: "plus.app.fill")
+                    Text("设置今日目标")
+                    Spacer()
+                }
+            } else {
+                RichTextViewer(content: comment)
+            }
+        }
+        .padding()
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10))
+        .frame(maxHeight: 180)
     }
 }
 
-#Preview {
-    struct Playground: View {
-        @State private var comment: String = "{\"ops\":[{\"attributes\":{\"bold\":true},\"insert\":\"今天的目标是：\"},{\"insert\":\"\\n完成今日目标的开发\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"给菲打个视频\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"}]}"
-        
-        var body: some View {
-            VStack {
-                TodayTarget(comment: comment)
-            }.padding()
-        }
-    }
+#Preview("normal") {
+    let comment = "{\"ops\":[{\"attributes\":{\"bold\":true},\"insert\":\"今天的目标是：\"},{\"insert\":\"\\n完成今日目标的开发\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"给菲打个视频\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"},{\"insert\":\"保持开心\"},{\"attributes\":{\"list\":\"unchecked\"},\"insert\":\"\\n\"}]}"
     
-    return Playground()
+    return TodayTarget(comment: comment).padding()
+}
+
+#Preview("empty") {
+    TodayTarget(comment: "")
+        .padding()
 }
