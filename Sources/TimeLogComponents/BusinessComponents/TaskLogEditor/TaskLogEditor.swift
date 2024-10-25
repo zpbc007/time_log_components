ba80b34a5bc9a4b63ecf11d9ed5f039c1f913c63
zpbc007 <719197352@qq.com>
2024-10-25 08:22:49 +0800

    test: add maxHeight

--- Sources/TimeLogComponents/BusinessComponents/TaskLogEditor/TaskLogEditor.swift
+++ Sources/TimeLogComponents/BusinessComponents/TaskLogEditor/TaskLogEditor.swift
@@ -66,7 +66,7 @@ public struct TaskLogEditor: View {
                     }
                 }
                 
-                RichTextEditor()
+                RichTextEditor(maxHeight: 200)
                     .frame(maxHeight: 200)
                 
                 DatePicker(
